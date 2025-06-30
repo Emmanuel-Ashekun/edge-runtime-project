1bc23dde7960df39afb809ee4664ee4855c0a98d
Emmy Devops <ashekunemmanuel@gmail.com>
2025-06-29 22:44:01 -0400

    updated app

--- docker/inference.py
+++ docker/inference.py
@@ -1,17 +1,5 @@
 # docker/inference.py
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello from the edge inference container!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-# docker/inference.py
-
 from flask import Flask, request
 import logging
 from logging.handlers import RotatingFileHandler
